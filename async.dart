172a074a8541ef294cb711261442f0da21bcf803
victormdev <victoralexandrino99@gmail.com>
2020-06-23 18:54:37 -0300

    create async

---
+++ async.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+
+main(){
+  print('Main program: Starts');
+  printFileContent();
+  print('Main program: Ends');
+
+}
+
+printFileContent(){
+  Future<String> fileContent = downloadAFile();
+  print('The content of the fila is --> $fileContent');
+}
+
+// Para baixar um arquivo [ Dummy long running operation ]
+Future<String> downloadAFile(){
+
+  Future<String> result = Future.delayed(Duration(seconds: 6), (){
+    return 'My secret file content';
+  });
+
+  return result;
+}
